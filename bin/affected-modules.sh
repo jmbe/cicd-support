9b0d0db93038378891600116bed5cecd80ba3b50
JM Bergqvist <jm.bergqvist@intem.se>
2018-11-11 18:22:40 +0100

    Initial import

---
+++ bin/affected-modules.sh
@@ -0,0 +1,54 @@
+#! /usr/bin/env bash
+
+revision=${1:-$CI_COMMIT_BEFORE_SHA}
+
+echo "Checking changes from revision ${revision} forwards..." >&2
+
+function upsearch() {
+
+  local x=$1;
+#  echo "Initial: $x"
+
+  while [ "$x" != "/" ] ;  do
+    #echo "Looking in $x"
+    if [ -e "$x/$2" ]; then
+       echo "$x"
+       break;
+    fi
+
+    x=$(dirname "$x");
+
+  done
+}
+
+# http://zaiste.net/2015/05/how_to_join_elements_of_an_array_in_bash/
+function join_s { local IFS="$1"; shift; echo "$*"; }
+
+function fpom() {
+  paths=()
+
+  while read -r line; do
+    # Collect paths piped to script
+    #echo "Read path $line" >&2
+    project=$(upsearch ${line} "pom.xml")
+    printf '%s\n' ${project}
+  done
+
+}
+
+function to_array() {
+  lines=()
+
+  while read -r line; do
+    lines+=(${line})
+  done
+
+  list=$(join_s "," "${lines[@]}")
+  echo ${list}
+}
+
+
+if [ ! -z "$revision" ]; then
+  modules=$(git diff --name-only ..${revision} | xargs --max-args=1 dirname | sort --unique | fpom | sort --unique | to_array)
+  echo "--also-make-dependents --projects ${modules}"
+fi
---
+++ bin/last-green-commit.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+if [[ -z "$GITLAB_PRIVATE_TOKEN" ]]; then
+    echo "GITLAB_PRIVATE_TOKEN not provided"
+    exit 1
+fi
+
+if [[ -z "$CI_PROJECT_ID" ]]; then
+    echo "CI_PROJECT_ID not provided"
+    exit 2
+fi
+
+url="${CI_SERVER_URL:-https://gitlab.com}/api/v4/projects/${CI_PROJECT_ID}/pipelines?private_token=${GITLAB_PRIVATE_TOKEN}&status=success&ref=${CI_COMMIT_REF_NAME}"
+#commit=$(curl -s "${url}" | jq -r 'first(.[] | .sha)' 2> /dev/null)
+commit=$(curl -s "${url}" | jq -r '.[0].sha' 2> /dev/null)
+
+if [[ ! -z "$commit" ]]; then
+    echo "$commit" # Used when capturing output of this command
+    echo "$commit" > .LAST_GREEN_COMMIT
+else
+    echo "Failed to find last green commit" >&2
+    which jq >&2
+    # curl --silent "${url}"
+fi
