eda57653714682db73c10d0da4fcdb0db9225250
JM Bergqvist <jm.bergqvist@intem.se>
2020-03-24 17:49:52 +0100

    Format scripts

--- bin/affected-modules.sh
+++ bin/affected-modules.sh
@@ -6,58 +6,61 @@ echo "Checking changes from revision ${revision} forwards..." >&2
 
 function upsearch() {
 
-  local x=$1;
-#  echo "Initial: $x"
+    local x=$1
+    #  echo "Initial: $x"
 
-  while [ "$x" != "/" ] ;  do
-    #echo "Looking in $x"
-    if [ -e "$x/$2" ]; then
-       echo "$x"
-       break;
-    fi
+    while [ "$x" != "/" ]; do
+        #echo "Looking in $x"
+        if [ -e "$x/$2" ]; then
+            echo "$x"
+            break
+        fi
 
-    x=$(dirname "$x");
+        x=$(dirname "$x")
 
-  done
+    done
 }
 
 # http://zaiste.net/2015/05/how_to_join_elements_of_an_array_in_bash/
-function join_s { local IFS="$1"; shift; echo "$*"; }
+function join_s() {
+    local IFS="$1"
+    shift
+    echo "$*"
+}
 
 function findpom() {
-  paths=()
+    paths=()
 
-  while read -r line; do
-    # Collect paths piped to script
-    #echo "Read path $line" >&2
-    project=$(upsearch ${line} "pom.xml")
-    printf '%s\n' ${project}
-  done
+    while read -r line; do
+        # Collect paths piped to script
+        #echo "Read path $line" >&2
+        project=$(upsearch ${line} "pom.xml")
+        printf '%s\n' ${project}
+    done
 
 }
 
 function to_array() {
-  lines=()
+    lines=()
 
-  while read -r line; do
-    lines+=(${line})
-  done
+    while read -r line; do
+        lines+=(${line})
+    done
 
-  list=$(join_s "," "${lines[@]}")
-  echo ${list}
+    list=$(join_s "," "${lines[@]}")
+    echo ${list}
 }
 
-
 if [[ ! -z "$revision" ]] && [[ "$revision" != "0000000000000000000000000000000000000000" ]]; then
-  changedfiles=$(git diff --name-only ${revision}..HEAD | grep --extended-regexp --invert-match "^\.(idea|development|cicd|editorconfig|mvn)|\.(iml)$")
-  gitResult=$?
+    changedfiles=$(git diff --name-only ${revision}..HEAD | grep --extended-regexp --invert-match "^\.(idea|development|cicd|editorconfig|mvn)|\.(iml)$")
+    gitResult=$?
 
-  if [[ -z "${changedfiles}" ]]; then
-    echo "List of changed files was empty for revision ${revision}. Likely only support files have changed." >&2
-  elif [[ ${gitResult} -eq 0 ]]; then
-    modules=$(echo ${changedfiles} | xargs --max-args=1 dirname | sort --unique | findpom | sort --unique | to_array)
-    echo "--also-make-dependents --projects ${modules}"
-  else
-    echo "Failed to find changed files comparing revision ${revision} with HEAD. It has possibly been removed." >&2
-  fi
+    if [[ -z "${changedfiles}" ]]; then
+        echo "List of changed files was empty for revision ${revision}. Likely only support files have changed." >&2
+    elif [[ ${gitResult} -eq 0 ]]; then
+        modules=$(echo ${changedfiles} | xargs --max-args=1 dirname | sort --unique | findpom | sort --unique | to_array)
+        echo "--also-make-dependents --projects ${modules}"
+    else
+        echo "Failed to find changed files comparing revision ${revision} with HEAD. It has possibly been removed." >&2
+    fi
 fi
--- bin/last-green-commit.sh
+++ bin/last-green-commit.sh
@@ -12,12 +12,12 @@ fi
 
 url="${CI_SERVER_URL:-https://gitlab.com}/api/v4/projects/${CI_PROJECT_ID}/pipelines?private_token=${GITLAB_PRIVATE_TOKEN}&status=success&ref=${CI_COMMIT_REF_NAME}"
 #commit=$(curl -s "${url}" | jq -r 'first(.[] | .sha)' 2> /dev/null)
-commit=$(curl -s "${url}" | jq -r '.[0].sha' 2> /dev/null)
+commit=$(curl -s "${url}" | jq -r '.[0].sha' 2>/dev/null)
 
 # Will say "null" when there are no previous green builds
 if [[ ! -z "$commit" ]] && [[ "$commit" != "null" ]]; then
     echo "$commit" # Used when capturing output of this command
-    echo "$commit" > .LAST_GREEN_COMMIT
+    echo "$commit" >.LAST_GREEN_COMMIT
 else
     echo "Failed to find last green commit" >&2
     which jq >&2
