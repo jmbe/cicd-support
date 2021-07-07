100d0577865558f6620085207eacf8ae4d9ffb85
JM Bergqvist <jm.bergqvist@intem.se>
2021-07-07 23:42:29 +0200

    Move helm-upgrade.sh to submodule

---
+++ bin/helm-upgrade.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+
+# Wrapper which only runs upgrade for existing releases.
+# Takes same arguments as helm upgrade, except release-name must be provided first.
+
+releaseName=$1
+shift
+
+if [ -z "$1" ]; then
+  echo "Release name must be provided"
+fi
+
+if helm ls --short | grep "^${releaseName}$" >/dev/null; then
+  echo "Upgrading $releaseName..."
+  helm upgrade "$releaseName" "$@"
+else
+  echo "Could not find release $releaseName. Skipping."
+  exit 0 # This script is intentionally skipping some projects, so do not fail build
+fi
