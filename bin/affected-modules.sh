391c8bb54faa06d421a9d85f3487b8a97bdad44d
JM Bergqvist <jm.bergqvist@intem.se>
2022-03-19 14:16:04 +0100

    Support providing external list of ignored paths

--- bin/affected-modules.sh
+++ bin/affected-modules.sh
@@ -51,9 +51,14 @@ function to_array() {
     echo ${list}
 }
 
+uninteresting="^\.(idea|development|cicd|editorconfig)|\.(iml)$"
+if [ -n "$IGNORED_PATHS_REGEXP" ]; then
+    uninteresting="$uninteresting|$IGNORED_PATHS_REGEXP"
+fi
+
 if [[ ! -z "$revision" ]] && [[ "$revision" != "0000000000000000000000000000000000000000" ]]; then
     changedfiles=$(git diff --name-only -z ${revision}..HEAD | \
-    grep --null-data --null --extended-regexp --invert-match "^\.(idea|development|cicd|editorconfig)|\.(iml)$" | \
+    grep --null-data --null --extended-regexp --invert-match "$uninteresting" | \
     xargs --null dirname | \
     sort --unique | \
     findpom | \
