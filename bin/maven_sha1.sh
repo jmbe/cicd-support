2f227be94b69ea18fbd7cfd03ceb7e38eca7c743
JM Bergqvist <jm.bergqvist@intem.se>
2022-09-21 23:08:32 +0200

    Fix issue determining branch name on GitLab

--- bin/maven_sha1.sh
+++ bin/maven_sha1.sh
@@ -4,6 +4,9 @@
 # This is supposed to match https://gitlab.com/intem-oss/maven-config-branchtool/-/blob/master/roles/maven-config-git-flow-hooks/files/post-flow-feature-start
 # for feature branches, while also using an empty sha1 for common trunk branch names.
 
-FEATURE_SHA1=$(git rev-parse --abbrev-ref HEAD | sed 's#feature/##' | sed 's#[-/]#_#g' | sed --regexp-extended 's#^(develop|master|main)$##')
+# The script assumes this will be run on GitLab where CI_COMMIT_REF_NAME is available. Note that GitLab will use
+# a detached head, so git rev-parse will not work to find branch name (https://gitlab.com/gitlab-org/gitlab/-/issues/15409).
+
+FEATURE_SHA1=$(echo $CI_COMMIT_REF_NAME | sed 's#feature/##' | sed 's#[-/]#_#g' | sed --regexp-extended 's#^(develop|master|main)$##')
 
 echo $FEATURE_SHA1
