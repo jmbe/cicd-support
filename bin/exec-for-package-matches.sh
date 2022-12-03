3a54e75f7a59e2aa7303b294e197461cf7dcb187
JM Bergqvist <jm.bergqvist@intem.se>
2022-12-03 14:41:13 +0100

    Let bash scripts delegate to deno scripts if found

--- bin/exec-for-package-matches.sh
+++ bin/exec-for-package-matches.sh
@@ -1,5 +1,13 @@
 #! /usr/bin/env bash
 
+# Delegate to deno implementation if deno can be found
+if [ -x "$(command -v deno)" ]; then
+    # This indirection is required for Ubuntu 18.04. With newer versions the ts file can be called directly
+    deno run --allow-read --allow-run $(dirname $0)/exec-for-package-matches.ts $*
+    exit $?
+fi
+
+# Fall back to legacy script implementation if deno cannot be found
 if [ -z "$2" ]; then
     echo "Usage: $(basename $0) package command-to-execute..."
     echo
--- bin/install-yarn-packages.sh
+++ bin/install-yarn-packages.sh
@@ -1,5 +1,13 @@
 #!/usr/bin/env bash
 
+# Delegate to deno implementation if deno can be found
+if [ -x "$(command -v deno)" ]; then
+    # This indirection is required for Ubuntu 18.04. With newer versions the ts file can be called directly
+    deno run --allow-read --allow-run $(dirname $0)/install-yarn-packages.ts
+    exit $?
+fi
+
+# Fall back to legacy script implementation if deno cannot be found
 {
     find . -not \( -path "*/node_modules/*" -prune -o -path "*/node/*" -prune \) -a \( -name yarn.lock -o -name package.json \)
 } | grep --invert-match /node_modules/ \
