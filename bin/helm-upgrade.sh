95e8138fb5ab460453ae7cfa9a7468d8a78285b1
JM Bergqvist <jm.bergqvist@intem.se>
2023-10-15 23:44:46 +0200

    Verify helm upgrade arguments have been provided

--- bin/helm-upgrade.sh
+++ bin/helm-upgrade.sh
@@ -7,13 +7,14 @@ releaseName=$1
 shift
 
 if [ -z "$1" ]; then
-  echo "Release name must be provided"
+  echo "helm upgrade arguments cannot be empty"
+  exit 1
 fi
 
 if helm ls --all --short | grep "^${releaseName}$" >/dev/null; then
   echo "Upgrading $releaseName..."
   helm upgrade "$releaseName" "$@"
 else
-  echo "Could not find release $releaseName. Skipping."
+  echo "Could not find release $releaseName. Skipping upgrade."
   exit 0 # This script is intentionally skipping some projects, so do not fail build
 fi
