e6eb9f9effadbe250307cca55423cf0828e20e0b
JM Bergqvist <jm.bergqvist@intem.se>
2024-05-09 18:57:15 +0200

    Revert CI_JOB_TOKEN spike

--- bin/last-green-commit.sh
+++ bin/last-green-commit.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
 
-# Prefer using pipeline-provided CI_JOB_TOKEN or fall back to GITLAB_PRIVATE_TOKEN
-CI_JOB_TOKEN=${CI_JOB_TOKEN:-GITLAB_PRIVATE_TOKEN}
-
-if [[ -z "$CI_JOB_TOKEN" ]]; then
-    echo "CI_JOB_TOKEN not provided" >&2
+# When testing locally, provide a personal access token with scope api
+# https://gitlab.com/-/user_settings/personal_access_tokens
+#
+# Note that the pipeline-provided CI_JOB_TOKEN is not able to access the required API in GitLab 17.0 and
+# gives 401 errors. A separate token must be configured in CI/CD environment variables for the group or project.
+# https://docs.gitlab.com/ee/ci/jobs/ci_job_token.html
+if [[ -z "$GITLAB_PRIVATE_TOKEN" ]]; then
+    echo "GITLAB_PRIVATE_TOKEN not provided" >&2
     exit 1
 fi
 
@@ -15,7 +18,7 @@ fi
 
 url="${CI_SERVER_URL:-https://gitlab.com}/api/v4/projects/${CI_PROJECT_ID}/pipelines?status=success&ref=${CI_COMMIT_REF_NAME}"
 #commit=$(curl -s "${url}" | jq -r 'first(.[] | .sha)' 2> /dev/null)
-commit=$(curl --header "PRIVATE-TOKEN: ${CI_JOB_TOKEN}" -s "${url}" | jq -r '.[0].sha' 2>/dev/null)
+commit=$(curl --header "PRIVATE-TOKEN: ${GITLAB_PRIVATE_TOKEN}" -s "${url}" | jq -r '.[0].sha' 2>/dev/null)
 
 # Will say "null" when there are no previous green builds
 if [[ ! -z "$commit" ]] && [[ "$commit" != "null" ]]; then
