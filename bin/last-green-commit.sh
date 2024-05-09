8d625689c4616676f08a0f1ec16b8c0e4a9ffe04
JM Bergqvist <jm.bergqvist@intem.se>
2024-05-09 18:23:29 +0200

    Provide token using header

--- bin/last-green-commit.sh
+++ bin/last-green-commit.sh
@@ -13,9 +13,9 @@ if [[ -z "$CI_PROJECT_ID" ]]; then
     exit 2
 fi
 
-url="${CI_SERVER_URL:-https://gitlab.com}/api/v4/projects/${CI_PROJECT_ID}/pipelines?private_token=${CI_JOB_TOKEN}&status=success&ref=${CI_COMMIT_REF_NAME}"
+url="${CI_SERVER_URL:-https://gitlab.com}/api/v4/projects/${CI_PROJECT_ID}/pipelines?status=success&ref=${CI_COMMIT_REF_NAME}"
 #commit=$(curl -s "${url}" | jq -r 'first(.[] | .sha)' 2> /dev/null)
-commit=$(curl -s "${url}" | jq -r '.[0].sha' 2>/dev/null)
+commit=$(curl --header "PRIVATE-TOKEN: ${CI_JOB_TOKEN}" -s "${url}" | jq -r '.[0].sha' 2>/dev/null)
 
 # Will say "null" when there are no previous green builds
 if [[ ! -z "$commit" ]] && [[ "$commit" != "null" ]]; then
