1aa96b3a113b9e4af0101b020602e64ea6c681b8
JM Bergqvist <jm.bergqvist@intem.se>
2025-12-14 19:47:57 +0100

    Support additional package managers for recursive install script

--- bin/install-yarn-packages.ts
+++ bin/install-yarn-packages.ts
@@ -2,12 +2,26 @@
 
 import os from "https://deno.land/x/dos@v0.11.0/mod.ts";
 
-function createCommand() {
+// TODO rename this and related files to install-packages.ts
+
+function createCommand(packageManager: string) {
   if (os.platform() === "windows") {
-    return ["cmd", "/c", "yarn"];
+    return ["cmd", "/c", packageManager, "install"];
   }
 
-  return ["yarn"];
+  return [packageManager, "install"];
+}
+
+function lockFileToPackageManager(lockFile: string) {
+  if (lockFile === "yarn.lock") {
+    return "yarn";
+  } else if (lockFile === "pnpm-lock.yaml") {
+    return "pnpm";
+  } else if (lockFile === "package-lock.json") {
+    return "npm";
+  } else {
+    throw `Unknown lock file: ${lockFile}`;
+  }
 }
 
 const ignoredDirectories = [
@@ -29,11 +43,11 @@ async function traverse(currentPath: string) {
       if (!ignoredDirectories.includes(dirEntry.name)) {
         await traverse(entryPath);
       }
-    } else if ("yarn.lock" === dirEntry.name) {
-      console.log(`Found yarn.lock at ${Deno.cwd()}/${entryPath}`);
+    } else if (["yarn.lock", "pnpm-lock.yaml"].includes(dirEntry.name)) {
+      console.log(`Found ${dirEntry.name} at ${Deno.cwd()}/${entryPath}`);
 
       const process = Deno.run({
-        cmd: createCommand(),
+        cmd: createCommand(lockFileToPackageManager(dirEntry.name)),
         cwd: currentPath,
       });
 
